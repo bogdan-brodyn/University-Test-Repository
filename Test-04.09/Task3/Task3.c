a51d076bc6839a51511e2c0e52344248bb037d77
bogdan-brodyn <mashina0oshibok@gmail.com>
2023-10-04 15:20:38 +0300

    Add third test's task

---
+++ Test-04.09/Task3/Task3.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    FILE* file;
+    errno_t errorCode = fopen_s(&file, "test.txt", "r");
+    if (file == NULL) {
+        printf("file not found!");
+        return 1;
+    }
+
+    char nextChar = 0;
+    bool flag = false;
+    while (true)
+    {
+        nextChar = fgetc(file);
+        if (nextChar == EOF)
+        {
+            break;
+        }
+
+        if (flag)
+        {
+            printf("%c", nextChar);
+            if (nextChar == '\n')
+            {
+                flag = false;
+            }
+        }
+        else if (nextChar == ';')
+        {
+            flag = true;
+        }
+    }
+    fclose(file);
+}
